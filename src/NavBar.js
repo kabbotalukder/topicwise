505e024fd69245a7295d3a34176462dc1618922f
Tamiz Ullah Kabbo <kabbotalukder5@gmail.com>
2023-08-13 17:04:16 +0600

    bug fixes

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,9 +6,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                <h1 style={{ color: '#2196f3' }}>TopicWise</h1>
+                <h1 className='project-title' style={{ color: '#2196f3' }}>TopicWise</h1>
             </div>
-                <p className='developer'>Developed by <a className='dev-link' href='https://facebook.com/kabbotalukder'>Kabbo Talukder</a></p>
+            <p className='developer'>Developed by <a target='_blank' className='dev-link' href='https://facebook.com/kabbotalukderr'>Kabbo Talukder</a></p>
         </nav>
     );
 }
