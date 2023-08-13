89a99ad8fba4a295b79cfaeb903191a84e32d063
Tamiz Ullah Kabbo <kabbotalukder5@gmail.com>
2023-08-13 16:54:37 +0600

    few changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,6 +8,7 @@ function Navbar() {
             <div className="navbar-logo">
                 <h1 style={{ color: '#2196f3' }}>TopicWise</h1>
             </div>
+                <p className='developer'>Developed by <a className='dev-link' href='https://facebook.com/kabbotalukder'>Kabbo Talukder</a></p>
         </nav>
     );
 }
